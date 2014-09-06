e8df7b8828c7420d1f25d71ed4e68b9a6d89f4f4
Michael Rodrigues <mikebrodrigues@gmail.com>
2014-09-06 00:20:21 -0700

    added #to_array, #to_hash, and #== to Host

--- lib/arp_scan/host.rb
+++ lib/arp_scan/host.rb
@@ -7,5 +7,24 @@ module ARPScan
       @mac = mac
       @oui = oui
     end
+
+    def to_hash
+      { :ip_addr => @ip_addr,
+        :mac     => @mac,
+        :oui      => @oui
+      }
+    end
+
+    def to_array
+      [ @ip_addr,
+        @mac,
+        @oui
+      ]
+    end
+
+    def ==(other)
+      self.to_array == other.to_array ? true : false
+    end
+
   end
 end
