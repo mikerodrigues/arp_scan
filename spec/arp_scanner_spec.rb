6f9b63c330022dd77e522a2771169d484139697a
Michael Rodrigues <mikebrodrigues@gmail.com>
2015-04-05 23:49:16 -0700

    fixed indentation on test

--- spec/arp_scanner_spec.rb
+++ spec/arp_scanner_spec.rb
@@ -10,17 +10,18 @@ module ARPScan
       it "raises an error if passed invalid scan arguments" do
         expect { ARPScanner.scan('invalid arguments')}.to raise_error
       end
+    end
 
-      describe "#which" do
-        it "returns a path for the binary if found" do
-          expect(ARPScanner.send(:which, 'ping').class).to eq(String)
-        end
-
-        it "raises an error if no matching binary is found" do
-          expect { ARPScanner.send(:which, 'ireallyhopeyoudonthavethisbin') }.to raise_error
-        end
+    describe "#which" do
+      it "returns a path for the binary if found" do
+        expect(ARPScanner.send(:which, 'ping').class).to eq(String)
       end
 
+      it "raises an error if no matching binary is found" do
+        expect { ARPScanner.send(:which, 'ireallyhopeyoudonthavethisbin') }.to raise_error
+      end
     end
+
   end
+end
 end
